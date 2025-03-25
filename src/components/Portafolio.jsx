825e06ce03cb6e38c3edf391ad4e1435a0bf438e
martinoremonje <m.monjeorellana@gmail.com>
2025-03-24 22:05:46 -0300

    portfolio

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -67,8 +67,13 @@ const Portfolio = () => {
 
           <h2 className="text-3xl font-semibold mb-4">Certificados</h2>
           <ul className="list-disc ml-5">
-            <li>Bootcamp en Talento Digital</li>
-            <li>Certificación React</li>
+            <li><a href="https://www.acreditta.com/credential/cd07f906-5bd4-4424-a954-87ff634ff493?utm_source=linkedin_profile&resource_type=badge&resource=cd07f906-5bd4-4424-a954-87ff634ff493">BOOTCAMP DESARROLLO DE APLICACIONES FULL STACK JAVASCRIPT TRAINEE</a></li>
+            <li><a href="https://www.udemy.com/certificate/UC-d4877a13-f82b-447e-93ff-efed00299161/">Curso React y Next.js: Aprende Frontend y Backend FullStack</a></li>
+            <li><a href="https://desafiosdev.s3.amazonaws.com/uploads/certification/image/565/aprobacion-final-td8-g68-m10-desarrollo-de-aplicaciones-full-stack-javascript-trainee-v2-0-desarrollo-de-empleabilidad-en-la-industria-digital-ele030792068087-375(2).png">Desarrollo de aplicaciones Full Stack Javascript Trainee</a></li>
+            <li><a href="https://sqlinteractivo.desafiolatam.com/certificados/685e5192-557a-4279-b6a5-109bbb51dd5e">SQL Interactivo</a></li>
+            <li><a href="https://www.udemy.com/certificate/UC-d289abb7-989d-4720-8ad4-9f383f3cde3e/">Git, GitHub, GitLab y GitHub Copilot: Uso de repositorios</a></li>
+            <li><a href="https://www.udemy.com/certificate/UC-a940e831-9611-4483-a2a0-40c4ba5e326d/">Universidad Desarrollo Web - FrontEnd Web Developer!</a></li>
+            <li><a href="https://cert.efset.org/oi5sET">EFSET English Certificate 82/100 (C2 Proficient)</a></li>
           </ul>
         </div>
         
@@ -100,15 +105,25 @@ const Portfolio = () => {
             </li>
           </ul>
         </div>
+        <div  style={{
+    borderRadius: '30px', // Ajusta el nivel de redondez.
+    overflow: 'hidden', // Recorta el contenido que exceda los bordes.
+    width: '30vw', // Ajusta el tamaño del contenedor según el GIF.
+    height: '30vh', // Define la altura del contenedor.
+  }}>
+
+
         <div
-          className="tenor-gif-embed"
-          data-postid="25845145"
+          className="tenor-gif-embed rounded-lg"
+          data-postid="14012925"
           data-share-method="host"
           data-aspect-ratio="1"
-          data-width="30%"
-          style={{ minWidth: '300px', marginLeft: '1rem' }}
+          data-width="60%"
+          style={{ width: '100%',
+            height: '100%',
+            objectFit: 'cover' }}
         >
-          <a href="https://tenor.com/view/email-email-marketing-glockapps-email-tool-gif-25845145">
+          <a href="https://tenor.com/view/aa-gif-25149805">
             GIF de Email Marketing
           </a>
         </div>
@@ -117,6 +132,7 @@ const Portfolio = () => {
           async
           src="https://tenor.com/embed.js"
         />
+  </div>
       </section>
     </div>
   );
